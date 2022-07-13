cc51045a0854be3452f65e8e12e745471a056a93
Chris <chris.schwint@gmail.com>
2022-07-13 09:51:29 -0400

    edits to dapp style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,14 @@ import logo from '../images/logo.png'
 
 const Navbar = ({ web3Handler, account, explorerURL }) => {
     return (
-        <nav className="navbar fixed-top mx-0 my-2">
+        <nav className="navbar fixed-top mx-0 my-0">
             <a
                 className="navbar-brand col-sm-3 col-md-2 mr-0 mx-0"
                 href="https://www.neurokeep.com/getsmarterdescription"
                 target="_blank"
                 rel="noopener noreferrer"
             >
-                <img src={logo} className="App-logo" alt="logo" />
+                <img src={logo} className="App-logo" alt="logo"/>
                 Smart Shades
             </a>
 
@@ -22,7 +22,7 @@ const Navbar = ({ web3Handler, account, explorerURL }) => {
                     {account.slice(0, 5) + '...' + account.slice(38, 42)}
                 </a>
             ) : (
-                <button onClick={web3Handler} className="button nav-button btn-sm mx-2">Connect Wallet</button>
+                <button onClick={web3Handler} className="button nav-button btn-sm mx-1">Connect Wallet</button>
             )}
         </nav>
     )
